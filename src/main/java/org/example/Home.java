8b818aac77625983c89cc399f3414629f54546f2
Pham Dang Khoa <107476994+Nibphamm@users.noreply.github.com>
2025-09-14 18:36:52 +0700

    Fix bugs HomeUC

--- src/main/java/org/example/Home.java
+++ src/main/java/org/example/Home.java
@@ -4,4 +4,7 @@ public class Home {
     public static void display() {
         System.out.println("This is the Home class.");
     }
+    public static void display(String[] args) {
+        System.out.println("Fix bugs cho thang lon ngu vcl");
+    }
 }
