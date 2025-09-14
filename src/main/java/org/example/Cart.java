469e1e90c320fc14f0f2aa874b6b2eb8a8834d8e
Pham Dang Khoa <107476994+Nibphamm@users.noreply.github.com>
2025-09-14 18:32:00 +0700

    Fix bugs CartUC

--- src/main/java/org/example/Cart.java
+++ src/main/java/org/example/Cart.java
@@ -3,5 +3,6 @@ package org.example;
 public class Cart {
     public static void display(String[] args) {
         System.out.println("This is the Cart class.");
+        System.out.println("Fix bugs and code smells in the Cart class.");
     }
 }
